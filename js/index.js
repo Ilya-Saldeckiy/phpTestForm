d33ad5bbcdb59abb2af56fdcc1f34d0a09194ab3
Илья <67915032+Ilya-Saldeckiy@users.noreply.github.com>
2021-06-11 16:46:43 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let registerCard = document.querySelector('.block__register')
+
+    registerCard.addEventListener('click', function(event){
+        registerCard.classList.add('block__register__active')
+    })
+
+
+
+
+})
